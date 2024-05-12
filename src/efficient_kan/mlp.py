969f35ab4185db9de94d8c63155c1a63944659af
Jianpan Huang <43700029+JianpanHuang@users.noreply.github.com>
2024-05-12 16:47:22 +0800

    Add files via upload

---
+++ src/efficient_kan/mlp.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+# Define MLP model architecture
+class MLP(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
