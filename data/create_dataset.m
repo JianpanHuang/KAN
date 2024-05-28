e02f9d9fd51bb30600c3728b4ee39680e0b27a32
Jianpan Huang <43700029+JianpanHuang@users.noreply.github.com>
2024-05-28 19:38:02 +0800

    Add files via upload

--- data/create_dataset.m
+++ data/create_dataset.m
@@ -1,7 +1,7 @@
 clear all;
 ds = 50000; % data size
 x=0:0.2:10;
-for n = 1:ds
+for n = 1:1
     a = randsample(20,1)/10;
     b = randsample(20,1)/10;
     c = randsample(20,1)/10;
@@ -11,8 +11,8 @@ for n = 1:ds
     input(:,n) = y;
     target(:,n) = [a,b,c,d];
 end
-% figure, plot(x,y)
+figure, plot(x,y)
 inputs = input';
 targets = target';
-csvwrite('inputs.csv', inputs);
-csvwrite('targets.csv', targets);
+csvwrite('test_input.csv', inputs);
+csvwrite('test_targe.csv', targets);
